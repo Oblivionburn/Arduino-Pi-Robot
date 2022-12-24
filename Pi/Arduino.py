0ab0360f82c46fecc75ec2ac1a2883109108cae1
Oblivionburn <donney.lane@gmail.com>
2022-12-24 05:58:39 -0600

    v0.6

---
+++ Pi/Arduino.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python 3
+import serial, time
+
+class Arduino:
+    def __init__(self, port: int, baudrate: int, connectAttemptsFail: int):
+        self.port = port
+        self.baudrate = baudrate
+        self.serial = None
+        self.connectAttempts = 0
+        self.connectAttemptsFail = connectAttemptsFail
+        self.Connect()
+
+    def Connect(self):
+        self.connectAttempts += 1
+        self.serial = serial.Serial("/dev/ttyACM" + str(self.port), self.baudrate, timeout = 1)
+        time.sleep(1) #wait for serial to open
+
+        if (self.IsConnected()):
+            self.connectAttempts = 0
+            print("Connected to Arduino on port {}".format(self.port))
+            return True
+        elif (self.connectAttempts <= self.connectAttemptsFail):
+            print("Device not responding. Reconnecting...")
+            self.Reconnect()
+        else:
+            print("Failed to reconnect!")
+            return False
+
+    def IsConnected(self):
+        return self.serial.isOpen()
+
+    def Reconnect(self):
+        self.serial.close()
+        print("Closed port {}.".format(self.port))
+        time.sleep(1)
+        return self.Connect()
+
+    def GetMessage(self, timeout: int):
+        while (True):
+            try:
+                waiting = 0
+
+                if (timeout > 0 and waiting >= timeout):
+                    return None
+                elif (self.serial.inWaiting() > 0):
+                    return self.serial.readline().decode('utf-8').rstrip()
+                else:
+                    waiting += 1
+            except Exception as error:
+                print(error)
+                return None
+
+    def SendMessage(self, message: str):
+        while (True):
+            try:
+                self.serial.write(message.rstrip().encode('utf-8'))
+                print("Pi: " + message.rstrip())
+                return True
+            except Exception as error:
+                if (not self.IsConnected()):
+                    print("Lost connection to Arduino. Reconnecting...")
+                    self.Reconnect()
+                else:
+                    print(error)
+                    return False
+
+    def TurnMotor(self, motor: int, angle: int):
+        try:
+            message = "<rotate:motor"
+
+            if (motor < 0 or motor > 15):
+                raise Exception("Motor index must be 0-15")
+            elif (motor < 10):
+                message += "0" + str(motor) + ";"
+            else:
+                message += str(motor) + ";"
+            
+            if (angle < 0 or angle > 359):
+                raise ValueError("Angle must be 0-359")
+            else:
+                message += str(angle) + ">"
+
+            return self.SendMessage(message)
+        except Exception as error:
+            print(error.args)
--- Pi/motor-test.py
+++ Pi/motor-test.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python 3
-import debugpy
-debugpy.listen(5678)
-
-import serial, time
+from Arduino import Arduino
+import time
 
 sent = False
 reset = False
@@ -15,107 +13,58 @@ send = ""
 received = ""
 
 if (__name__ == '__main__'):
-    while reset == False:
+    while (not reset):
         try:
-            arduino = serial.Serial("/dev/ttyACM" + str(port), 9600, timeout=1)
-            time.sleep(1) #wait for serial to open
-
-            if arduino.isOpen():
-                print("{} connected!".format(arduino.port))
-                while True:
+            arduino = Arduino(port, 9600, 10)
+            if (arduino.IsConnected()):
+                while (True):
                     try:
-                        while (arduino.inWaiting == 0): pass
-                        if arduino.inWaiting() > 0:
-                            received = arduino.readline().decode('utf-8').rstrip()
+                        received = arduino.GetMessage(60)
 
-                            if ("Finished" in received):
-                                print(received + "\n")
+                        if ("Finished" in received):
+                            print(received + "\n")
 
-                                if (reset == False):
-                                    angle += 10
+                            if (not reset):
+                                angle += 10
 
-                                    if (angle > 180):
-                                        angle = 0
-                                        motor += 1
-
-                                        if (motor == 6):
-                                            angle = 0
-                                            motor = 0
-                                            reset = True
-                                else:
+                                if (angle > 180):
+                                    angle = 0
                                     motor += 1
 
-                                    if (motor == 6):
-                                        break
+                                    if (motor == 4):
+                                        angle = 0
+                                        motor = 0
+                                        reset = True
+                            else:
+                                motor += 1
 
-                                sent = False
-                                received = ""
+                                if (motor == 4):
+                                    break
 
-                            elif ("Arduino:" in received):
-                                print(received)
-                                received = ""
+                            sent = False
+                            received = ""
 
-                            if (sent == False):
-                                if (reset == True):
-                                    send = "<rotate:motor0" + str(motor) + ";0>"
-                                    arduino.write(send.encode('utf-8'))
-                                    print("Resetting motor " + str(motor))
-                                else:
-                                    send = "<rotate:motor0" + str(motor) + ";" + str(angle) + ">"
-                                    arduino.write(send.encode('utf-8'))
-                                    print("Sent: " + send.rstrip())
+                        elif ("Arduino:" in received):
+                            print(received)
+                            received = ""
 
-                                sent = True
-                        elif (sent == True):
-                            time.sleep(0.01)
-                            waiting += 1
-                            if (waiting == 1000):
-                               print("Re-sending...")
-                               arduino.flush()
-                               send = "<rotate:motor0" + str(motor) + ";" + str(angle) + ">"
-                               arduino.write(send.encode('utf-8'))
-                               print("Sent: " + send.rstrip())
-                               time.sleep(1)
-                               waiting = 0
+                        if (not sent):
+                            if (reset):
+                                arduino.TurnMotor(motor, 0)
+                            else:
+                                arduino.TurnMotor(motor, angle)
+
+                            sent = True
                     except serial.serialutil.SerialException:
                         print("No response from device.")
 
-                        if (arduino.isOpen()):
+                        if (arduino.IsConnected()):
                             print("Still connected. Waiting for response...")
                             time.sleep(1)
                         else:
-                            print("Device not responding. Re-connecting...")
-                            arduino.close()
-                            print("Closed port...")
-                            time.sleep(1)
-                            arduino = serial.Serial("/dev/ttyACM" + str(port), 9600, timeout=1)
-                            print("Re-opened port...")
-
-                            if (arduino.isOpen()):
-                                print("{} connected!".format(arduino.port))
-                                except_count = 0
-                            else:
-                                except_count += 1
-                                if (except_count == 5):
-                                    print("Failed to re-connect.")
-                                    break
+                            arduino.Reconnect()
                     except OSError:
-                        print("Device not ready...")
-                        arduino.close()
-                        print("Closed port...")
-                        port = 0
-                        time.sleep(1)
-                        arduino = serial.Serial("/dev/ttyACM" + str(port), 9600, timeout=1)
-                        print("Re-opened port...")
-
-                        if (arduino.isOpen()):
-                            print("{} connected!".format(arduino.port))
-                            except_count = 0
-                        else:
-                            except_count += 1
-                            if (except_count == 5):
-                                print("Failed to re-connect.")
-                                break
+                        arduino.Reconnect()
         except OSError:
             print("Port " + str(port) + " not found...")
             port += 1
